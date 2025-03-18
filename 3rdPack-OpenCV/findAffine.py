4f799dd5107f25c3b4f68295e338fbd93e0d70e8
StefanosTzaf <stzaferis04@gmail.com>
2025-03-18 21:39:46 +0100

    initial

---
+++ 3rdPack-OpenCV/findAffine.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('photos/lena.png')
+height, width = img.shape[:2]
+cv2.imshow("Original", img)
+cv2.waitKey(0)
+
+
+pts_1 = np.float32([[135, 45], [385, 45], [135, 230]])
+pts_2 = np.float32([[135, 45], [385, 45], [150, 230]])
+M = cv2.getAffineTransform(pts_1, pts_2)
+
+dst = cv2.warpAffine(img, M, (1024, 1024))
+cv2.imshow("Affine Transformation", dst)
+cv2.waitKey(0)
---
+++ 3rdPack-OpenCV/rotation.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('photos/dnd.jpg')
+# πρωτα το υψος!
+height, width = img.shape[:2]
+print(width)
+print(height)
+cv2.imshow("Original", img)
+cv2.waitKey(0)
+
+#double // for flour division -- center (width ~ height) , angle, scale
+M = cv2.getRotationMatrix2D( (width//2.0, height//2.0) , 90, 1)
+dst = cv2.warpAffine(img, M, (width, height))
+cv2.imshow("Rotated", dst)
+cv2.waitKey(0)
